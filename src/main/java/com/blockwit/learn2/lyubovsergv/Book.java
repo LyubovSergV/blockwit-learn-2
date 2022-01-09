fbf6f7315b92ecccc193192b1bd106877f3ed450
LyubovSergV <lyubovsergeeva95@gmail.com>
2022-01-09 19:11:54 +0300

    Task62 и Planet

---
+++ src/main/java/com/blockwit/learn2/lyubovsergv/Book.java
@@ -0,0 +1,26 @@
+package com.blockwit.learn2.lyubovsergv;
+
+public class Book {
+    String name, author;
+    int birthday;
+
+    public Book(String inputName, String inputAuthor, int inputBirthday) {
+        name = inputName;
+        author = inputAuthor;
+        birthday = inputBirthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getBirth() {
+        return birthday;
+    }
+
+
+}
