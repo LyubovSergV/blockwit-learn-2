ba8f0a0cc81d3fc0eb53108349ee4067cd8f25c3
LyubovSergV <lyubovsergeeva95@gmail.com>
2022-01-09 19:14:05 +0300

    Task63 и Book

--- src/main/java/com/blockwit/learn2/lyubovsergv/Task63.java
+++ src/main/java/com/blockwit/learn2/lyubovsergv/Task63.java
@@ -3,7 +3,7 @@ package com.blockwit.learn2.lyubovsergv;
 public class Task63 {
     public static void main(String[] args) {
         Book maugli = new Book("Maygli", "Kipling", 1894);
-        System.out.println("Название " + maugli.getName());
+        System.out.println("Название книги " + maugli.getName());
         System.out.println("Автор " + maugli.getAuthor());
         System.out.println("Год " + maugli.getBirth());
     }
