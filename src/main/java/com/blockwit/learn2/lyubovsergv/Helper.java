7f82bea91a1344d5a522ac99e935baff1c575059
LyubovSergV <lyubovsergeeva95@gmail.com>
2022-01-14 18:33:00 +0300

    2 задача

---
+++ src/main/java/com/blockwit/learn2/lyubovsergv/Helper.java
@@ -0,0 +1,15 @@
+package com.blockwit.learn2.lyubovsergv;
+
+public class Helper {
+    int numbers;
+
+    public Helper(int inputNembers){
+
+        numbers=inputNembers;
+    }
+
+    public void printIntArray(){
+        System.out.println(numbers);
+
+    }
+}
---
+++ src/main/java/com/blockwit/learn2/lyubovsergv/tu1.java
@@ -0,0 +1,19 @@
+package com.blockwit.learn2.lyubovsergv;
+
+public class tu1 {
+    public static void main(String[] args) {
+        Helper[] numbers = {
+                new Helper(12),
+                new Helper(7),
+                new Helper(89),
+                new Helper(-4),
+                new Helper(8),
+                new Helper(9)
+        };
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i].printIntArray();
+        }
+
+    }
+}
