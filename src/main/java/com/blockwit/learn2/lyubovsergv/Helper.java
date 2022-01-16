46cfe99501cfeab188c09742745138507c11dec2
LyubovSergV <lyubovsergeeva95@gmail.com>
2022-01-16 17:34:55 +0300

    задача 2

--- src/main/java/com/blockwit/learn2/lyubovsergv/Helper.java
+++ src/main/java/com/blockwit/learn2/lyubovsergv/Helper.java
@@ -1,9 +1,15 @@
 package com.blockwit.learn2.lyubovsergv;
 
 public class Helper {
-    public static void printIntArray(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
+    int numbers;
+
+    public Helper(int inputNumbers) {
+
+        numbers = inputNumbers;
+    }
+
+    public void printIntArray() {
+        System.out.println(numbers);
+
     }
 }
--- src/main/java/com/blockwit/learn2/lyubovsergv/tu1.java
+++ src/main/java/com/blockwit/learn2/lyubovsergv/tu1.java
@@ -2,7 +2,17 @@ package com.blockwit.learn2.lyubovsergv;
 
 public class tu1 {
     public static void main(String[] args) {
-        System.out.println("Исходный массив");
-        Helper.printIntArray(new int[]{12, 7, 89, -4, 8, 9});
+        Helper[] numbers = {
+                new Helper(12),
+                new Helper(7),
+                new Helper(89),
+                new Helper(-4),
+                new Helper(8),
+                new Helper(9)
+        };
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i].printIntArray();
+        }
     }
 }
