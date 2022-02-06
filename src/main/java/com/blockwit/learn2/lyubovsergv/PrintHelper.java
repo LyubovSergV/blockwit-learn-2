697d17e66988f4825090178ad2cd4e01d61db677
LyubovSergV <lyubovsergeeva95@gmail.com>
2022-02-07 00:14:57 +0300

    TaskS8T2

---
+++ src/main/java/com/blockwit/learn2/lyubovsergv/PrintHelper.java
@@ -0,0 +1,27 @@
+package com.blockwit.learn2.lyubovsergv;
+
+public class PrintHelper {
+    public void swapFirstAndLastElement(String[] names){
+        String tempName = names[0];
+        names[0] = names[names.length - 1];
+        names[names.length - 1] = tempName;
+    }
+    public void printArrayOfStrings(String[] arrayOfString){
+        for(int i=0;i<arrayOfString.length;i++){
+            System.out.println(arrayOfString[i]);
+        }
+    }
+
+    public void printArrayOfInteger(int[] arrayOfInteger){
+        for(int i = 0; i<arrayOfInteger.length; i++){
+            System.out.println(arrayOfInteger[i]);
+        }
+    }
+    public void printInList(int[] arrayOfInteger){
+        for(int i = 0; i<arrayOfInteger.length; i++){
+            System.out.println(arrayOfInteger[i]);
+        }
+    }
+
+
+}
---
+++ src/main/java/com/blockwit/learn2/lyubovsergv/TaskS8T2.java
@@ -0,0 +1,21 @@
+package com.blockwit.learn2.lyubovsergv;
+
+public class TaskS8T2 {
+    public static void main(String[] args) {
+        MyIntArrayList myIntArrayList = new MyIntArrayList();
+
+        PrintHelper printHelper = new PrintHelper();
+
+        for (int i = 0; i < 5; i++) {
+            myIntArrayList.add(i);
+        }
+
+        System.out.println("Список");
+        printHelper.printInList(myIntArrayList.elements);
+
+        System.out.println("Очищенный Список");
+        for (int i = 0; i < myIntArrayList.size(); i++) {
+            System.out.println(myIntArrayList.clear());
+        }
+    }
+}
